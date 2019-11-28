d4abe61079eb3ac281cd91c13038f17df28b4c7d
Ubuntu <ubuntu@ip-172-31-20-220.ap-southeast-1.compute.internal>
2019-11-28 02:51:25 +0000

    Added q3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Box from './Box.js'
 import Counter from './Counter.js'
+import BMI from './BMI.js'
 function App() {
   return (
     <div>
-    <Box/>
-    <Counter/>
+      <BMI/>
     </div>
   );
 }
---
+++ src/BMI.js
@@ -0,0 +1,58 @@
+import React from "react"
+
+export default class BMI extends React.Component {
+    // state variables
+    state = {
+        weight: 0.0,
+        height: 0.0
+    }
+    
+    constructor(props) {
+        super(props);
+        this.updateWeight = this.updateWeight.bind(this);
+        this.updateHeight = this.updateHeight.bind(this);
+        this.calculateBMI = this.calculateBMI.bind(this);
+    }
+    
+    updateWeight(event) {
+        let newWeight = event.target.value;
+        this.setState({
+            weight:newWeight
+        })
+    }
+    
+    updateHeight(event) {
+        let newHeight = event.target.value;
+        this.setState({
+            height:newHeight
+        })
+    }
+    
+    calculateBMI(event) {
+        let weight = parseFloat(this.state.weight);
+        let height = parseFloat(this.state.height);
+        let bmi = weight / (height * height);
+        alert(bmi);
+    }
+    
+    render() {
+        return (
+            <div>
+                <input type='number' 
+                 value={this.state.weight} 
+                 placeholder='Weight'
+                 onChange={this.updateWeight}
+                 />
+                <br/>
+                
+                <input type='number' 
+                value={this.state.height} 
+                placeholder='Height'
+                onChange={this.updateHeight}
+                />
+                
+                <button onClick={this.calculateBMI}> Calculate</button>
+            </div>
+        )
+    }
+}
